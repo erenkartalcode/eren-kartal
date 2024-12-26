c4a9432ad7841c9d983ad696c44b5d2fc79a89cb
erenkartalcode <eren.kartal@std.yeditepe.edu.tr>
2024-12-26 16:32:24 +0300

    Eren Kartal
    Übung_1

---
+++ erenkartal.py
@@ -0,0 +1,6 @@
+alt=int(input("Bitte geben Sie seine alt ab!"))
+absch=int(input("Bitte geben Sie seine Abschlussnote ab!"))
+if 20<=alt<=50 and absch>=80:
+ print("Einstellen")
+else:
+ print("Ablehnen")
